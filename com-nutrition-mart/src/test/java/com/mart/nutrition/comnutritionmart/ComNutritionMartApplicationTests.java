2914a0af12b33267e98e03b72c65316be7bec81b
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-07-28 23:03:07 +0530

    unit test for hello controller

--- com-nutrition-mart/src/test/java/com/mart/nutrition/comnutritionmart/ComNutritionMartApplicationTests.java
+++ com-nutrition-mart/src/test/java/com/mart/nutrition/comnutritionmart/ComNutritionMartApplicationTests.java
@@ -1,13 +1,28 @@
 package com.mart.nutrition.comnutritionmart;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ComNutritionMartApplicationTests {
 
+  @Autowired
+  private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void TestHello() {
+    webTestClient
+      .get().uri("/hello")
+      .accept(MediaType.TEXT_PLAIN)
+      .exchange()
+      .expectStatus().isOk()
+      .expectBody(String.class).isEqualTo("Hello, Spring!");
 	}
 
 }
