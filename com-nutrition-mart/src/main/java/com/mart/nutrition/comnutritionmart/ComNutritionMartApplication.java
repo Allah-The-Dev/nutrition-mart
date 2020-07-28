534fbae51fbfa6776749fa33f3dde8a14c5c3e5b
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-07-28 19:58:44 +0530

    backend first commit

---
+++ com-nutrition-mart/src/main/java/com/mart/nutrition/comnutritionmart/ComNutritionMartApplication.java
@@ -0,0 +1,13 @@
+package com.mart.nutrition.comnutritionmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComNutritionMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComNutritionMartApplication.class, args);
+	}
+
+}
---
+++ com-nutrition-mart/src/test/java/com/mart/nutrition/comnutritionmart/ComNutritionMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mart.nutrition.comnutritionmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComNutritionMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
